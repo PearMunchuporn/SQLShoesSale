409f631874e76cd5b5b99c7268a81f8b06e3bd72
Munchuporn <111532818+PearMunchuporn@users.noreply.github.com>
2025-04-25 15:32:22 +0700

    Update Reviews SQL Data Analysis.sql

--- Reviews SQL Data Analysis.sql
+++ Reviews SQL Data Analysis.sql
@@ -1,50 +1,54 @@
 ---What are products ranked top 3 highest ratings?
 
-with Highest_Rating as (
+WITH Highest_Rating as (
 SELECT TOP (1000)
-      b.[product_id]
-	  ,b.[brand]
-	 , i.[product_name]
-	 ,r.[rating]
-     ,dense_rank() over(order by [rating] desc ) as rank_rating   
+     b.[product_id]
+    ,b.[brand]
+    ,i.[product_name]
+    ,r.[rating]
+    ,DENSE_RANK() OVER (ORDER BY [rating] DESC ) as rank_rating   
   FROM [Shoes].[dbo].[finance] f
-  join [Shoes].[dbo].[reviews] r
-  on f.product_id = r.product_id
-  join [Shoes].[dbo].[info] i
-  on i.product_id = f.product_id
-  join [Shoes].[dbo].[brands] b
-  on b.product_id = f.product_id)
-  select [product_id] ,[brand] ,[product_name],[rating],rank_rating
-  from Highest_Rating
-  where rank_rating<4
+  JOIN [Shoes].[dbo].[reviews] r
+  ON f.[product_id] = r.[product_id]
+  JOIN [Shoes].[dbo].[info] i
+  ON i.[product_id] = f.[product_id]
+  JOIN [Shoes].[dbo].[brands] b
+  ON b.[product_id] = f.[product_id]
+)
+  SELECT [product_id], [brand], [product_name], [rating], rank_rating
+  FROM Highest_Rating
+  WHERE rank_rating < 4
 
-
+	
 ---What are products ranked top 3 lowest ratings?
-with Lowest_Rating as (
+	
+WITH Lowest_Rating as (
 SELECT TOP (1000)
       b.[product_id]
-	  ,b.[brand]
-	 , i.[product_name]
-	 ,r.[rating]
-	 ,f.[revenue]
-	, r.[reviews]
-	,dense_rank() over(order by [rating]  ) as rank_rating
+     ,b.[brand]
+     ,i.[product_name]
+     ,r.[rating]
+     ,f.[revenue]
+     ,r.[reviews]
+     ,DENSE_RANK() OVER (ORDER BY [rating]) as rank_rating
 
   FROM [Shoes].[dbo].[finance] f
-  join [Shoes].[dbo].[reviews] r
-  on f.product_id = r.product_id
-  join [Shoes].[dbo].[info] i
-  on i.product_id = f.product_id
-  join [Shoes].[dbo].[brands] b
-  on b.product_id = f.product_id)
-
-  select [product_id] ,[brand] ,[product_name],[rating],rank_rating  ,reviews
-  from Lowest_Rating
-  where rank_rating<4 and revenue <> 0 
+  JOIN [Shoes].[dbo].[reviews] r
+  ON f.[product_id] = r.[product_id]
+  JOIN [Shoes].[dbo].[info] i
+  ON i.[product_id] = f.[product_id]
+  JOIN [Shoes].[dbo].[brands] b
+  ON b.[product_id] = f.[product_id]
+)
+  SELECT [product_id], [brand], [product_name], [rating], rank_rating, [reviews]
+  FROM Lowest_Rating
+  WHERE rank_rating < 4 and revenue <> 0
+	
 	
 --Find products have sold but got 0 rating.
 --How many products got 0 rating.
 -- ((70 Adidas shoes got a 0 rating))
+	
 SELECT 
       b.[brand]
       ,count(r.[product_id]) as [number of rated products]
@@ -59,30 +63,31 @@ SELECT
   on f.product_id = r.product_id
   where rating =0 and revenue!=0  group by rating,brand 
 	
+	
 --Find products have sold but got 0 rating.
 --Categorize ratings of the products by criteria 5 is Excellent, less than 5 but more than or equal 4 is Good,  less than 4 but more than or equal 3 is Fair, less than 3 but more than or equal 2 is Poor,  less than 2 but more than or equal 1 is Bad, 0 rating is Worst.
+
 SELECT 
-     
       b.[brand] 
-	 ,i.[product_name]
+     ,i.[product_name]
      ,r.[rating]
-	 ,case
-	 when rating = 5  then 'Excellent'
-	 when rating >=4 then 'Good'
-	 when rating >= 3 then 'Fair'
-	 when rating >= 2 then 'Poor'
-	 when  rating >= 1 then 'Bad'
-	 else  'Worse'
-	 end rating_result
+	 ,CASE
+	 WHEN rating = 5  then 'Excellent'
+	 WHEN rating >= 4 then 'Good'
+	 WHEN rating >= 3 then 'Fair'
+	 WHEN rating >= 2 then 'Poor'
+	 WHEN rating >= 1 then 'Bad'
+	 ELSE  'Worse'
+	 END rating_result
 
   FROM [Shoes].[dbo].[reviews] r
-  join [Shoes].[dbo].[brands] b
-  on r.product_id = b.product_id
-  join [Shoes].[dbo].[info] i
-  on i.product_id = r.product_id
-  join [Shoes].[dbo].[finance] f
-  on f.product_id = r.product_id
-  where revenue !=0
+  JOIN [Shoes].[dbo].[brands] b
+  ON r.[product_id] = b.[product_id]
+  JOIN [Shoes].[dbo].[info] i
+  ON i.[product_id] = r.[product_id]
+  JOIN [Shoes].[dbo].[finance] f
+  ON f.[product_id] = r.[product_id]
+  WHERE revenue != 0
 
 
 ---Number of rated products by critiria
@@ -92,42 +97,43 @@ SELECT
 --- Poor is 163
 --- Worst is 70
 
-SELECT sub.rating_result ,count(ID) as [number of rated products] ,sub.rating_ranked 
+ SELECT sub.rating_result ,COUNT(ID) as [number of rated products] ,sub.rating_ranked 
  FROM(
 SELECT 
       r.[ID]
      ,r.[rating]
-	 ,case
-	 when rating = 5  then 'Excellent'
-	 when rating >=4 then 'Good'
-	 when rating >= 3 then 'Fair'
-	 when rating >= 2 then 'Poor'
-	 when  rating >= 1 then 'Bad'
-	 else  'Worst'
-	 end rating_result
-	 ,case
-	 when rating = 5  then 1
-	 when rating >=4 then 2
-	 when rating >= 3 then 3
-	 when rating >= 2 then 4
-	 when  rating >= 1 then 5
-	 else  6
-	 end as rating_ranked
-	 ,count(r.[id]) as [number_of_rated_products]
+  ,CASE
+	 WHEN rating = 5  THEN 'Excellent'
+	 WHEN rating >= 4 THEN 'Good'
+	 WHEN rating >= 3 THEN 'Fair'
+         WHEN rating >= 2 THEN 'Poor'
+	 WHEN rating >= 1 THEN 'Bad'
+	 ELSE 'Worst'
+	 END rating_result
+
+  ,CASE
+	 WHEN rating = 5  THEN 1
+	 WHEN rating >= 4 THEN 2
+	 WHEN rating >= 3 THEN 3
+	 WHEN rating >= 2 THEN 4
+	 WHEN rating >= 1 THEN 5
+	 ELSE  6
+	 END as rating_ranked
+        ,COUNT(r.[id]) as [number_of_rated_products]
 
   FROM [Shoes].[dbo].[reviews] r
-  join [Shoes].[dbo].[brands] b
-  on r.product_id = b.product_id
-  join [Shoes].[dbo].[info] i
-  on i.product_id = r.product_id
-  join [Shoes].[dbo].[finance] f
-  on f.product_id = r.product_id
+  JOIN [Shoes].[dbo].[brands] b
+  ON r.product_id = b.product_id
+  JOIN [Shoes].[dbo].[info] i
+  ON i.product_id = r.product_id
+  JOIN [Shoes].[dbo].[finance] f
+  ON f.product_id = r.product_id
 
-  where revenue !=0
+  WHERE revenue != 0
  
-  group by [rating], r.[ID]
+  GROUP BY [rating], r.[ID]
 
   )as sub
-group by rating_result  ,rating_ranked
-order by sub.rating_ranked
+GROUP BY rating_result  ,rating_ranked
+ORDER BY sub.rating_ranked
 
